42d833c1a25d5e159f64054e3ca37124ad6fe694
Glenn Wedin <glenn.wedin@capgemini.com>
2016-12-30 15:46:44 +0100

    build settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin'),
+CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = [{
 	name: "client",
@@ -27,7 +28,14 @@ module.exports = [{
         }]
     },
 	plugins: [
-		new ExtractTextPlugin("../dist/css/[name].css")
+		new ExtractTextPlugin("../dist/css/[name].css"),
+    new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+    })
 	]
 }, {
 	name: "server",
