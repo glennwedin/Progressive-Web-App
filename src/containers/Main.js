befb33708d29fae21dadb4fb1e3777af344d2a9c
Glenn Wedin <glenn_wedin@hotmail.com>
2017-03-04 23:40:53 +0100

    react-router-dom

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router";
+import { Route } from "react-router-dom";
 import { Provider, observer } from 'mobx-react';
 import Top from '../components/Top';
 import Menu from '../components/Menu';
